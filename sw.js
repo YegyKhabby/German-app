8acb9fdbcb7c459668c7ff50bd4e37ad9382a8ec
Yeganeh Khabbazian <yeganehkhabbazian@gmail.com>
2026-04-26 10:04:10 +0200

    fix: add network error fallback to service worker fetch handler

--- sw.js
+++ sw.js
@@ -22,5 +22,9 @@ self.addEventListener("activate", e => {
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request)
+      .then(cached => cached || fetch(e.request))
+      .catch(() => caches.match("/index.html"))
+  );
 });
